a3b19b010b2eb3f3a64a1dbc0a07ada7746e8fa9
dengjiecheng <1048641228@qq.com>
2017-03-21 14:30:21 +0800

    main activity

---
+++ app/src/main/java/com/iflytek/aiuiproduct/Main2Activity.java
@@ -0,0 +1,16 @@
+package com.iflytek.aiuiproduct;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.iflytek.aiuiproduct.demo.R;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
